6fe0abeaf745da068bcd3144da0db2556383e827
Willian Garcia <webwil2012@gmail.com>
2024-01-31 07:57:06 -0300

    Aplicando conteúdo e css

--- Alura02/js/script.js
+++ Alura02/js/script.js
@@ -1,7 +1,8 @@
 function imprimirNoBrowser() {
     armazenaTexto('title', 'Portifólio');
-    armazenaTexto('h1', 'texto H1');
-    armazenaTexto('p', 'Texto Paragrafo');
+    armazenaTexto('h1', 'Eleve seu negócio digital a outro nível <strong></strong>');
+    armazenaTexto('strong', 'com um Front-end de qualidade!');
+    armazenaTexto('p', 'Olá! Sou Joana Santos, desenvolvedora Front-end com especialidade em React, HTML e CSS. Ajudo pequenos negócios e designers a colocarem em prática boas ideias. Vamos conversar?');
 }
 
 function armazenaTexto(tag, texto) {
