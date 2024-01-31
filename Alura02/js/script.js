de2b5d5002bb6fe4639051bd47125400b9431196
Willian Garcia <webwil2012@gmail.com>
2024-01-31 13:16:31 -0300

    Refactorando CSS

--- Alura02/js/script.js
+++ Alura02/js/script.js
@@ -1,6 +1,6 @@
 function imprimirNoBrowser() {
     armazenaTexto('title', 'Portifólio');
-    armazenaTexto('h1', 'Eleve seu negócio digital a outro nível <strong></strong>');
+    armazenaTexto('h1', 'Eleve seu negócio digital a outro nível <strong class="titulo-destaque"></strong>');
     armazenaTexto('strong', 'com um Front-end de qualidade!');
     armazenaTexto('p', 'Olá! Sou Joana Santos, desenvolvedora Front-end com especialidade em React, HTML e CSS. Ajudo pequenos negócios e designers a colocarem em prática boas ideias. Vamos conversar?');
 }
